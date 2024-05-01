5d64a1d1d087c273b1b8d60928cf465a2cd351ce
Thor Kampefner <thorck@pm.me>
2024-05-01 11:24:33 -0700

    rmv unnec

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,3 @@
 #![allow(non_snake_case)]
 #![allow(clippy::clone_on_copy)]
 #![allow(unused_mut)]
-
-mod error;
-#[cfg(test)] mod tests;
-mod utils;
-
-use error::MyError;
-use log::info;
-
-fn main() -> Result<(), MyError> {
-  utils::setup()?;
-
-  info!("hello thor");
-
-  Ok(())
-}
