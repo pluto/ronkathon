f07ff06f52aa6446f86aecdf182ef8efb65becce
Waylon Jepsen <57912727+0xJepsen@users.noreply.github.com>
2024-05-02 13:06:31 -0700

    feat: tests and edge cases

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@
 #![allow(non_snake_case)]
 #![allow(clippy::clone_on_copy)]
 #![allow(unused_mut)]
+#![allow(clippy::needless_range_loop)]
 
 pub mod curve;
 pub mod field;
--- src/polynomial.rs
+++ src/polynomial.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use p3_field::Field;
 
 /// A polynomial of degree N-1
@@ -7,6 +9,7 @@ pub struct Polynomial<const N: usize, B: Basis, F: Field> {
   pub basis:        B,
 }
 
+// Type state patern for polynomial bases:
 pub trait Basis {
   type Data;
 }
@@ -15,11 +18,14 @@ pub struct Monomial;
 impl Basis for Monomial {
   type Data = ();
 }
+
+// https://en.wikipedia.org/wiki/Lagrange_polynomial
 pub struct Lagrange<const N: usize, F: Field> {
   pub nodes:   [F; N],
   pub weights: [F; N],
   pub L:       Box<dyn Fn(F) -> F>,
 }
+
 impl<const N: usize, F: Field> Basis for Lagrange<N, F> {
   type Data = Self;
 }
@@ -27,11 +33,19 @@ impl<const N: usize, F: Field> Basis for Lagrange<N, F> {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Nodes<const N: usize, F: Field>([F; N]);
 
+/// A polynomial in monomial basis
 impl<const N: usize, F: Field> Polynomial<N, Monomial, F> {
   pub fn new(coefficients: [F; N]) -> Self { Self { coefficients, basis: Monomial } }
 
   /// Convert a polynomial from monomial to Lagrange basis using the Barycentric form
   pub fn to_lagrange(&self, nodes: Nodes<N, F>) -> Polynomial<N, Lagrange<N, F>, F> {
+    // check that nodes are distinct
+    let mut set = HashSet::new();
+    for &node in nodes.0.iter() {
+      if !set.insert(node) {
+        panic!("Nodes must be distinct");
+      }
+    }
     let mut coeffs = [F::one(); N];
     for j in 0..N {
       coeffs[j] = self.evaluate(nodes.0[j]);
@@ -39,6 +53,7 @@ impl<const N: usize, F: Field> Polynomial<N, Monomial, F> {
     Polynomial::<N, Lagrange<N, F>, F>::new(coeffs, nodes.0)
   }
 
+  /// Evaluate the polynomial at field element x
   pub fn evaluate(&self, x: F) -> F {
     let mut result = F::zero();
     for (i, c) in self.coefficients.into_iter().enumerate() {
@@ -47,6 +62,8 @@ impl<const N: usize, F: Field> Polynomial<N, Monomial, F> {
     result
   }
 }
+
+/// A polynomial in Lagrange basis
 impl<const N: usize, F: Field> Polynomial<N, Lagrange<N, F>, F> {
   pub fn new(coefficients: [F; N], nodes: [F; N]) -> Self {
     let mut weights = [F::one(); N];
@@ -67,6 +84,10 @@ impl<const N: usize, F: Field> Polynomial<N, Lagrange<N, F>, F> {
     let L = move |x: F| {
       let mut val = F::zero();
       for j in 0..N {
+        // check if we are dividing by zero
+        if nodes[j] == x {
+          return coefficients[j];
+        }
         val += coefficients[j] * weights[j] / (x - nodes[j]);
       }
       l(x) * val
@@ -76,8 +97,67 @@ impl<const N: usize, F: Field> Polynomial<N, Lagrange<N, F>, F> {
     Self { coefficients, basis }
   }
 
+  /// Evaluate the polynomial at field element x
   pub fn evaluate(&self, x: F) -> F {
     let L = &self.basis.L;
     L(x)
+  }
+}
+
+mod test {
+  use super::*;
+  use crate::field::PlutoField;
+
+  #[test]
+  fn evaluation() {
+    // Coefficients of the polynomial 1 + 2x + 3x^2
+    let a = PlutoField::new(1);
+    let b = PlutoField::new(2);
+    let c = PlutoField::new(3);
+    let polynomial = Polynomial::<3, Monomial, PlutoField>::new([a, b, c]);
+    let y = polynomial.evaluate(PlutoField::new(2));
+    let r = PlutoField::new(17);
+    assert_eq!(y, r); // 1 + 2*(2) + 3(2)^2 = 17
+  }
+
+  #[test]
+  fn evaluation_with_zero() {
+    // Coefficients of the polynomial 1 + 3x^2
+    let a = PlutoField::new(1);
+    let b = PlutoField::new(0);
+    let c = PlutoField::new(3);
+    let polynomial = Polynomial::<3, Monomial, PlutoField>::new([a, b, c]);
+    let y = polynomial.evaluate(PlutoField::new(0));
+    let r = PlutoField::new(1);
+    assert_eq!(y, r); // 1 + 3(0)^2 = 1
+  }
+
+  #[test]
+  fn lagrange_evaluation() {
+    // Coefficients of the polynomial 1 + 2x + 3x^2
+    let a = PlutoField::new(1);
+    let b = PlutoField::new(2);
+    let c = PlutoField::new(3);
+    let polynomial = Polynomial::<3, Monomial, PlutoField>::new([a, b, c]);
+
+    // Nodes for the Lagrange basis
+    let nodes =
+      Nodes::<3, PlutoField>([PlutoField::new(1), PlutoField::new(2), PlutoField::new(3)]);
+    let lagrange = polynomial.to_lagrange(nodes);
+    let r = lagrange.evaluate(PlutoField::new(2));
+    assert_eq!(r, PlutoField::new(17));
+  }
+
+  #[test]
+  #[should_panic]
+  fn non_unique_nodes() {
+    // Coefficients of the polynomial 1 + 2x
+    let a = PlutoField::new(1);
+    let b = PlutoField::new(2);
+
+    let polynomial = Polynomial::<2, Monomial, PlutoField>::new([a, b]);
+    // This should panic because the nodes are not distinct
+    let nodes = Nodes::<2, PlutoField>([PlutoField::new(1), PlutoField::new(1)]);
+    let lagrange = polynomial.to_lagrange(nodes);
   }
 }
