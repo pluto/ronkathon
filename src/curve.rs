ea9da62131048e00ec2c3f7aa458dde2df4e4a8a
Thor 🪁 <7041313+thor314@users.noreply.github.com>
2024-05-02 18:50:39 -0700

    curve implementation updated (#21)
    * C101 curve struct; CurveParams implemented for C101
    
    * impl logic for curve c101
    
    * fix colin comment
    
    * freaking fmt
    
    ---------
    
    Co-authored-by: Waylon Jepsen <57912727+0xJepsen@users.noreply.github.com>
    Co-authored-by: Waylon Jepsen <waylonjepsen1@gmail.com>

--- src/curve.rs
+++ src/curve.rs
@@ -1,6 +1,11 @@
-use std::ops::Add;
+use std::{
+  fmt,
+  ops::{Add, Neg},
+};
 
 use p3_field::{AbstractField, Field};
+
+use crate::field::PlutoField;
 
 /// Elliptic curve in Weierstrass form: y^2 = x^3 + ax + b
 pub struct Curve<F: Field> {
@@ -10,81 +15,102 @@ pub struct Curve<F: Field> {
   two:   F,
 }
 
-pub struct CurvePoint<F: Field> {
-  pub curve: Curve<F>,
-  pub point: PointOrInfinity<F>,
+pub trait CurveParams: 'static + Copy + Clone + fmt::Debug + Default + Eq + Ord {
+  /// Integer field element type
+  type FieldElement: Field + Neg;
+  /// Order of this elliptic curve, i.e. number of elements in the scalar field.
+  const ORDER: u32;
+  /// Coefficient `a` in the Weierstrass equation of this elliptic curve.
+  const EQUATION_A: Self::FieldElement;
+  /// Coefficient `b` in the Weierstrass equation of this elliptic curve.
+  const EQUATION_B: Self::FieldElement;
+  /// Generator of this elliptic curve.
+  const GENERATOR: (Self::FieldElement, Self::FieldElement);
+  // hack: 3 and 2 to satisfy the Add<AffinePoint> trait implementation
+  const THREE: Self::FieldElement;
+  const TWO: Self::FieldElement;
+
+  // maybe Curve::uint type is diff from PCP::fieldelement type
+  // maybe:
+  // type AffinePoint;
+  // type ProjectivePoint;
+  // type Scalar;
 }
 
-#[derive(Clone, Copy)]
-pub struct Point<F: Field> {
-  x: F,
-  y: F,
+/// The Elliptic curve $y^2=x^3+3$, i.e.
+/// - a = 0
+/// - b = 3
+/// - generator todo
+/// - order todo
+/// - field element type todo, but mock with u64 - bad thor, u64 does not implement p3_field
+#[derive(Copy, Clone, Debug, Default, Eq, PartialEq, PartialOrd, Ord)]
+pub struct C101;
+
+impl CurveParams for C101 {
+  type FieldElement = crate::field::PlutoField;
+
+  const EQUATION_A: Self::FieldElement = PlutoField::const_new(0);
+  const EQUATION_B: Self::FieldElement = PlutoField::const_new(3);
+  const GENERATOR: (Self::FieldElement, Self::FieldElement) = todo!();
+  const ORDER: u32 = PlutoField::ORDER_U32;
+  const THREE: Self::FieldElement = PlutoField::const_new(3);
+  const TWO: Self::FieldElement = PlutoField::const_new(2);
 }
 
-#[derive(Clone, Copy)]
-pub enum PointOrInfinity<F: Field> {
-  Point(Point<F>),
-  Infinity,
+/// An Affine Coordinate Point on a Weierstrass elliptic curve
+#[derive(Clone, Debug, Copy)]
+pub enum AffinePoint<C: CurveParams> {
+  XY(C::FieldElement, C::FieldElement),
+  Infty,
 }
 
-impl<F: Field> Curve<F> {
-  pub fn new(a: F, b: F) -> Self {
-    Self {
-      a,
-      b,
-      three: <F as AbstractField>::from_canonical_u8(3),
-      two: <F as AbstractField>::from_canonical_u8(2),
+impl<C: CurveParams> AffinePoint<C> {
+  pub fn new(x: C::FieldElement, y: C::FieldElement) -> Self {
+    assert_eq!(y * y, x * x * x + C::EQUATION_A * x + C::EQUATION_B, "Point is not on curve");
+    Self::XY(x, y)
+  }
+
+  pub fn new_infty() -> Self { Self::Infty }
+
+  pub fn negate(&mut self) {
+    match self {
+      Self::XY(_x, ref mut y) => *y = -*y,
+      Self::Infty => (),
     }
   }
 }
 
-impl<F: Field> CurvePoint<F> {
-  pub fn new(curve: Curve<F>, x: F, y: F) -> Self {
-    assert_eq!(y * y, x * x * x + curve.a * x + curve.b, "Point is not on curve");
-    CurvePoint { curve, point: PointOrInfinity::Point(Point { x, y }) }
-  }
+impl<C: CurveParams> Add for AffinePoint<C> {
+  type Output = AffinePoint<C>;
 
-  pub fn negate(&self, p: PointOrInfinity<F>) -> PointOrInfinity<F> {
-    match p {
-      PointOrInfinity::Point(p) => PointOrInfinity::Point(Point { x: p.x, y: -p.y }),
-      PointOrInfinity::Infinity => PointOrInfinity::Infinity,
+  fn add(self, rhs: Self) -> Self::Output {
+    // infty checks
+    match (self, rhs) {
+      (AffinePoint::Infty, _) => return rhs,
+      (_, AffinePoint::Infty) => return self,
+
+      _ => (),
     }
-  }
-
-  fn add_points(&self, p: Point<F>, q: Point<F>) -> Point<F> {
-    // https://en.wikipedia.org/wiki/Elliptic_curve_point_multiplicationcv
-    let (x_p, y_p) = (p.x, p.y);
-    let (x_q, y_q) = (q.x, q.y);
-
-    // check for zero
-    if x_p == x_q && y_p == -y_q {
-      return Point { x: F::zero(), y: F::zero() };
-    }
-
-    // Check if point is itself, if it is you double (which is easier)
-    let lamda = if x_p == x_q && y_p == y_q {
-      (self.curve.three * x_p * x_p + self.curve.a) / (self.curve.two * y_p)
-    } else {
-      (y_q - y_p) / (x_q - x_p)
+    let (x1, y1) = match self {
+      AffinePoint::XY(x, y) => (x, y),
+      AffinePoint::Infty => unreachable!(),
     };
-
-    let x = lamda * lamda - x_p - x_q;
-    let y = lamda * (x_p - x) - y_p;
-    Point { x, y }
-  }
-}
-
-impl<F: Field> Add for CurvePoint<F> {
-  type Output = CurvePoint<F>;
-
-  fn add(self, other: CurvePoint<F>) -> CurvePoint<F> {
-    match (self.point, other.point) {
-      (PointOrInfinity::Infinity, _) => other,
-      (_, PointOrInfinity::Infinity) => self,
-      (PointOrInfinity::Point(p), PointOrInfinity::Point(q)) => {
-        let r = self.add_points(p, q);
-        CurvePoint { curve: self.curve, point: PointOrInfinity::Point(Point { x: r.x, y: r.y }) }
-      },
+    let (x2, y2) = match rhs {
+      AffinePoint::XY(x, y) => (x, y),
+      AffinePoint::Infty => unreachable!(),
+    };
+    if x1 == x2 && y1 == -y2 {
+      return AffinePoint::new_infty();
     }
+    // compute new point using elliptic curve point group law
+    // https://en.wikipedia.org/wiki/Elliptic_curve_point_multiplication
+    let lambda = if x1 == x2 && y1 == y2 {
+      (C::THREE * x1 * x1 + C::EQUATION_A) / (C::TWO * y1)
+    } else {
+      (y2 - y1) / (x2 - x1)
+    };
+    let x = lambda * lambda - x1 - x2;
+    let y = lambda * (x1 - x) - y1;
+    AffinePoint::new(x, y)
   }
 }
