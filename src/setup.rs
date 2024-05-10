13c511bf2b2a57e860d60e0ec3b7bc12aa78c176
Waylon Jepsen <57912727+0xJepsen@users.noreply.github.com>
2024-05-09 19:18:56 -0600

    fix: no big int, but pow types

--- src/setup.rs
+++ src/setup.rs
@@ -3,19 +3,19 @@ use rand::{thread_rng, Rng};
 
 use crate::{
   curves::{g1_curve::C101, g2_curve::G2Curve, AffinePoint, CurveParams},
-  field::gf_101::GF101,
+  field::{gf_101::GF101, FiniteField},
 };
 
-fn setup(degree: u64) -> (Vec<AffinePoint<C101>>, Vec<AffinePoint<G2Curve>>) {
+fn setup(degree: u32) -> (Vec<AffinePoint<C101>>, Vec<AffinePoint<G2Curve>>) {
   // NOTE: For demonstration purposes only.
   // Create some toxic waste, typically done in MPC and discarded.
   let mut rng = thread_rng();
-  let mut tau_bytes = vec![0u8; 32];
+  let mut tau_bytes = [0u8; 4];
   rng.fill(&mut tau_bytes[..]);
-  println!("tau_toxic_waste={:?}", BigUint::from_bytes_le(&tau_bytes));
+  //   println!("tau_toxic_waste={:?}", BigUint::from_bytes_le(&tau_bytes));
 
   // This does get concatenated rn
-  let tau = BigUint::from_bytes_le(&tau_bytes);
+  let tau = GF101::from_canonical_u32(u32::from_le_bytes(tau_bytes));
 
   // let tau = Fr::rand(&mut rng);
 
@@ -25,15 +25,15 @@ fn setup(degree: u64) -> (Vec<AffinePoint<C101>>, Vec<AffinePoint<G2Curve>>) {
   // - in plonk, we need d+5 g1 elements and one g2 element
   let mut srs_g1_points: Vec<AffinePoint<C101>> = vec![];
   let mut srs_g2_points: Vec<AffinePoint<G2Curve>> = vec![];
-  for i in 1..degree + 1 {
-    // G1 Group
-    let result = AffinePoint::<C101>::generator() * tau.pow(i as u32);
-    srs_g1_points.push(result);
+  //   for i in 1..degree + 1 {
+  //     // G1 Group
+  //     let result = AffinePoint::<C101>::generator() *
+  // tau.pow(field::gf_101::GF101::Storage::from(i));     srs_g1_points.push(result);
 
-    // G2 Group
-    let result = AffinePoint::<G2Curve>::generator() * tau.pow(i as u32);
-    srs_g2_points.push(result);
-  }
+  //     // G2 Group
+  //     let result = AffinePoint::<G2Curve>::generator() * tau.pow(i as u32);
+  //     srs_g2_points.push(result);
+  //   }
 
   (srs_g1_points, srs_g2_points)
 }
