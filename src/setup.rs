c40da58a882809e376637d39fadb679291405177
Waylon Jepsen <57912727+0xJepsen@users.noreply.github.com>
2024-05-09 19:00:54 -0600

    wip: setup srs

---
+++ src/setup.rs
@@ -0,0 +1,50 @@
+use num_bigint::BigUint;
+use rand::{thread_rng, Rng};
+
+use crate::{
+  curves::{g1_curve::C101, g2_curve::G2Curve, AffinePoint, CurveParams},
+  field::gf_101::GF101,
+};
+
+fn setup(degree: u64) -> (Vec<AffinePoint<C101>>, Vec<AffinePoint<G2Curve>>) {
+  // NOTE: For demonstration purposes only.
+  // Create some toxic waste, typically done in MPC and discarded.
+  let mut rng = thread_rng();
+  let mut tau_bytes = vec![0u8; 32];
+  rng.fill(&mut tau_bytes[..]);
+  println!("tau_toxic_waste={:?}", BigUint::from_bytes_le(&tau_bytes));
+
+  // This does get concatenated rn
+  let tau = BigUint::from_bytes_le(&tau_bytes);
+
+  // let tau = Fr::rand(&mut rng);
+
+  // NOTE: Just sample the d of both for now.
+  // - g1 and g2 SRS have variable sizes for diff kzg uses
+  // - in eth blobs, g1 is 4096 elements, g2 is 16 elements
+  // - in plonk, we need d+5 g1 elements and one g2 element
+  let mut srs_g1_points: Vec<AffinePoint<C101>> = vec![];
+  let mut srs_g2_points: Vec<AffinePoint<G2Curve>> = vec![];
+  for i in 1..degree + 1 {
+    // G1 Group
+    let result = AffinePoint::<C101>::generator() * tau.pow(i as u32);
+    srs_g1_points.push(result);
+
+    // G2 Group
+    let result = AffinePoint::<G2Curve>::generator() * tau.pow(i as u32);
+    srs_g2_points.push(result);
+  }
+
+  (srs_g1_points, srs_g2_points)
+}
+
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_setup() {
+    let (g1, g2) = setup(5);
+    println!("{:?}", g1);
+    println!("{:?}", g2);
+  }
+}
