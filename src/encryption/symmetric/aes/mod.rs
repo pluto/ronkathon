cdf9ed0ab46d0225db43817b6c2f1d1950901bda
bing <eightfilms@keemail.me>
2024-06-28 17:28:55 +0800

    update example

--- src/encryption/symmetric/aes/mod.rs
+++ src/encryption/symmetric/aes/mod.rs
@@ -44,22 +44,13 @@ where [(); K / 8]:
   ///
   /// ## Example
   /// ```rust
+  /// use rand::{thread_rng, Rng};
   /// use ronkathon::encryption::symmetric::{aes::AES, SymmetricEncryption};
   ///
-  /// let key = Key::<128> {
-  ///    inner: [
-  ///      0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
-  ///      0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d,
-  ///      0x1e, 0x1f,
-  ///    ],
-  ///  };
-
-  ///  let plaintext = [
-  ///    0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee,
-  ///    0xff,
-  ///  ];
-
-  ///  let encrypted = AES::encrypt(&key, &plaintext);
+  /// let mut rng = thread_rng();
+  /// let key = Key::<128> { inner: rng.gen() };
+  /// let plaintext = rng.gen();
+  /// let encrypted = AES::encrypt(&key, &plaintext);
   /// ```
   fn encrypt(key: &Self::Key, plaintext: &Self::Block) -> Self::Block {
     let num_rounds = match K {
