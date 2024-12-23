e1122e0fd1c838e66f44ac08b8a2db4ead94cc52
Colin Roberts <colin@primitive.xyz>
2024-12-23 12:19:11 -0700

    feat: basic LWE

--- src/encryption/asymmetric/mod.rs
+++ src/encryption/asymmetric/mod.rs
@@ -1,2 +1,16 @@
 //! Contains implementation of asymmetric cryptographic primitives like RSA encryption.
+pub mod lwe;
 pub mod rsa;
+
+pub trait AsymmetricEncryption {
+  type PublicKey;
+  type PrivateKey;
+  type Plaintext;
+  type Ciphertext;
+
+  /// Encrypts plaintext using key and returns ciphertext
+  fn encrypt(&self, plaintext: &Self::Plaintext) -> Self::Ciphertext;
+
+  /// Decrypts ciphertext using key and returns plaintext
+  fn decrypt(&self, ciphertext: &Self::Ciphertext) -> Self::Plaintext;
+}
